27648a1bb2d84ec85c18e7a78a9296690af38def
geryan <geryan@users.noreply.github.com>
2025-02-17 15:02:37 +1100

    move in some existing functionality

--- _targets.R
+++ _targets.R
@@ -13,6 +13,18 @@ tar_source()
 
 list(
   tar_target(
+    dates,
+    seq.Date(
+      from = as.Date("2020-01-01"),
+      to = as.Date("2023-12-31"),
+      by = "day"
+    )
+  ),
 
+
+  tar_target(
+    pointless_end_target,
+    "pointless_end_target"
   )
+
 )
